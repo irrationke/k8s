336526deb35fce12222b020f57e4f3e78d29c18c
irrationke <kls_3127@163.com>
2023-12-31 20:47:53 +0800

    修改文件路径

--- pki/files/k8s_pki.sh
+++ pki/files/k8s_pki.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for NODE in k8s-master02 k8s-master03; do
-    for FILE in $(ls /etc/kubernetes/pki | grep -v etcd); do scp /etc/kubernetes/pki/${FILE} $NODE:/etc/kubernetes/pki/${FILE}; done
-    for FILE in admin.kubeconfig controller-manager.kubeconfig scheduler.kubeconfig; do scp /etc/kubernetes/${FILE} $NODE:/etc/kubernetes/${FILE}; done
+for NODE in 192.168.12.32 192.168.12.33 192.168.12.41 192.168.12.42; do
+    for FILE in $(ls /etc/kubernetes/pki | grep -v etcd); do
+        scp /etc/kubernetes/pki/${FILE} $NODE:/etc/kubernetes/pki/${FILE}
+    done
+    for FILE in admin.kubeconfig controller-manager.kubeconfig scheduler.kubeconfig; do
+        scp /etc/kubernetes/${FILE} $NODE:/etc/kubernetes/${FILE}
+    done
 done
