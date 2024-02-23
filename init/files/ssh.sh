9697819e87cde7f77b2eecb6ba8160ba4b7b8c18
irrationke <kls_3127@163.com>
2024-02-23 23:24:59 +0800

    变量注释

--- init/files/ssh.sh
+++ init/files/ssh.sh
@@ -4,7 +4,9 @@ if [ -e /root/.ssh/id_rsa ]; then
 else
      ssh-keygen -f /root/.ssh/id_rsa -P ''
 fi
+# 此处IP为Kubernetes集群的所有节点IP
 export IP="192.168.12.31 192.168.12.32 192.168.12.33 192.168.12.41 192.168.12.42"
+# 此处的密码为root的密码
 export SSHPASS=klx-=890
 for HOST in $IP; do
      sshpass -e ssh-copy-id -o StrictHostKeyChecking=no $HOST
