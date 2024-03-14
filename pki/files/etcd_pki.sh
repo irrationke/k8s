7a01867f3ba906101e7d92466cfeb43b9e57a8c4
klx-company <kls_3127@163.com>
2024-03-14 21:00:23 +0800

    更新变量名

--- pki/files/etcd_pki.sh
+++ pki/files/etcd_pki.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Master='192.168.12.32 192.168.12.33'
-for NODE in $Master; do
+Etcd='192.168.12.32 192.168.12.33'
+for NODE in $Etcd; do
     ssh  -o StrictHostKeyChecking=no $NODE "mkdir -p /etc/etcd/ssl"
     for FILE in etcd-ca-key.pem etcd-ca.pem etcd-key.pem etcd.pem; do
         scp -o StrictHostKeyChecking=no /etc/etcd/ssl/${FILE} $NODE:/etc/etcd/ssl/${FILE}
